4584df6492af2f64160ff83af9d628e86d4b4856
Alex Kanitz <alexander.kanitz@unibas.ch>
2020-07-26 13:17:22 +0200

    configure coverage and bump foca dependency

--- drs_filer/app.py
+++ drs_filer/app.py
@@ -1,9 +1,16 @@
+import os
+
 from foca.foca import foca
 
 
 def init():
     if __name__ == '__main__':
-        app = foca("config.yaml")
+        app = foca(
+            os.path.join(
+                os.path.dirname(__file__),
+                "config.yaml",
+            )
+        )
         app.run(port=app.port)
 
 
